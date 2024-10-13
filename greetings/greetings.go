0cd725a0ae8f1ab27cd756c5f036e79eab6f7b72
rxxx03 <floridaralf@riseup.net>
2024-10-13 11:01:45 +0200

    adds function to return random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,24 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("name is missing")
 	}
-	messsage := fmt.Sprintf("Hi, %v. Welcome!", name)
+	messsage := fmt.Sprintf(randomFormat(), name)
 	return messsage, nil
 }
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Cor blimey, guvner. 'ow's me old china %v?",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
+
